3f84312553b721816d12c5c6703372932e76106b
AlexanderYepez-code <alexsaomn17@gmail.com>
2026-01-14 17:35:02 +0100

    FEAT: primo documento completo

--- queries.sql
+++ queries.sql
@@ -11,3 +11,27 @@ SELECT *
 FROM `students`
 WHERE `date_of_birth` < "1996-01-14"
 ORDER BY `date_of_birth` DESC;
+select COUNT(*)
+from `students`;
+WHERE YEAR(NOW()) - YEAR(`date_of_birth`) < 30;
+-- Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di laurea (286)
+SELECT *
+FROM `courses`
+WHERE `period` = "I semestre"
+AND `year` = 1;
+-- Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del 20/06/2020 (21)
+SELECT COUNT(*)
+FROM `exams`
+WHERE `date` = "2020-06-20"
+AND `hour`> '14:00:00';
+-- Selezionare tutti i corsi di laurea magistrale (38)
+SELECT *
+FROM `degrees`
+WHERE `level` ="magistrale"
+-- Da quanti dipartimenti è composta l'università? (12)
+SELECT *
+FROM `departments`
+-- Quanti sono gli insegnanti che non hanno un numero di telefono? (50)
+SELECT COUNT(*)
+FROM `teachers`
+WHERE `phone` IS NULL
